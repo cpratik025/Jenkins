91f6f7fe5f557c0b49da913b84ca1aa530e44a24
Pratik <cpratik025@gmail.com>
2019-03-23 20:27:24 -0400

    Update HelloTest.java

--- java-maven-junit-helloworld-master/src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
+++ java-maven-junit-helloworld-master/src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
@@ -9,11 +9,11 @@ import java.io.PrintStream;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-
-  Unit test for Hello.
-  <p/>
-  A unit test aims to test all code and code paths of a specific class.
-
+/**
+ * Unit test for Hello.
+ * <p/>
+ * A unit test aims to test all code and code paths of a specific class.
+ */
 public class HelloTest {
 
     @Test
@@ -27,18 +27,20 @@ public class HelloTest {
     }
 
     @Test
-   public void testSayHelloAFewTimes() {
-       OutputStream os = new ByteArrayOutputStream();
-      PrintStream stream = new PrintStream(os, true);
-       Hello hi = new Hello();
-      hi.setTimes(3);
-     hi.sayHello(stream);
+    public void testSayHelloAFewTimes() {
+        OutputStream os = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(os, true);
 
-       // Does it say "Hello!" three times?
-      String goal = Hello.HELLO + "\n" + Hello.HELLO + "\n" + Hello.HELLO + "\n";
-     assertThat(os.toString(), is(equalTo(goal)));
+        Hello hi = new Hello();
+        hi.setTimes(3);
+        hi.sayHello(stream);
+
+        // Does it say "Hello!" three times?
+        String goal = Hello.HELLO + "\n" + Hello.HELLO + "\n" + Hello.HELLO + "\n";
+        assertThat(os.toString(), is(equalTo(goal)));
     }
-       @Test(expected = IllegalArgumentException.class)
+
+    @Test(expected = IllegalArgumentException.class)
     public void testIllegalArgumentForHello21() {
         Hello hi = new Hello();
         hi.setTimes(Hello.MAXIMUM_AMOUNT_OF_TIMES + 1);
